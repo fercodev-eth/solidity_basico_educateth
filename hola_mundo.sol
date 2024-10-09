fd240f817c39b29e6bc0abcd54ddc1550ad1574f
fercodev-eth <ferkymelc@gmail.com>
2024-10-08 20:16:05 -0400

    primer contrato

---
+++ hola_mundo.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26; // en esta paarte defino la version del compilador
+//0x4B6fa65E1451682b6aB0F78EDC58d081998eFBe9
+
+// a partir de aca pongo la logica del contrato
+contract hola_mundo {
+    string public saludo = "Hola desde Educateth 2.0"; //declarando el contenido  de mi variable saludo
+
+}
